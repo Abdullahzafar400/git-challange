c2d3c1d5dbb6486df3aef6a976bce6b176182646
Abdullah Zafar <abdullah.zafar@revbits.com>
2023-04-04 23:40:14 +0500

    feat: add base64 endpoint

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
   if (req.url === '/user-agent') return respondUserAgent(req, res)
+  if (req.url === '/base64') return respondBase64(req, res)
 
   res.end()
 })
@@ -14,6 +15,10 @@ function respondHello (req, res) {
 }
 function respondUserAgent (req, res) {
   res.end(JSON.stringify({ userAgent: req.headers['user-agent'] }))
+}
+
+function respondBase64 (req, res) {
+  res.end(JSON.stringify({ msg: Buffer.from("hello").toString('base64') }))
 }
 
 server.listen(PORT)
